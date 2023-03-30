2b1228ff04bcfe0717522972d9d064165904dc9e
user <user@email.com>
2023-03-30 18:31:11 +0200

    ~ Implemented MenuView logic

--- Assets/Project/Scripts/Views/MenuView.cs
+++ Assets/Project/Scripts/Views/MenuView.cs
@@ -1,10 +1,27 @@
+using TMPro;
 using UnityEngine;
 using TestTask_AnApp.Scripts.Abstractions;
 
 namespace TestTask_AnApp.Scripts.Views
 {
-    public class MenuView : ViewBase
-    {
+    public class MenuView : ViewBase 
+    { 
+        [SerializeField] private Money _money;
+        [Space]
+        [SerializeField] private TextMeshProUGUI _moneyText;
 
+        private void OnEnable()
+        {
+            _money.OnValueChange += ChangeMoneyValue;
+        }
+        private void OnDisable()
+        {
+            _money.OnValueChange -= ChangeMoneyValue;
+        }
+
+        private void ChangeMoneyValue(int money)
+        {
+            _moneyText.text = _money.Value.ToString();
+        }
     }
 }
